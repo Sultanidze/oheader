739976b09a55d40daae183ef2670742cb6c68daf
Sultanidze <Sultanidze@gmail.com>
2017-06-21 15:10:06 +0300

    SEO edits: show/hide menu without 'display: none', data-href on some links

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	// stick header
+	// stick header in top of window
 	$("#headerNew").stick_in_parent();
 
 	// menu hiding/appearance
@@ -7,7 +7,9 @@ $(document).ready(function(){
 		,$menuBtn = $("#menuBtn")
 		,$closeIcon = $menuBtn.find(".b-icon_bars")
 		,$openIcon = $menuBtn.find(".fa-times")
+		,menuHeight = $mainMenu.outerHeight();
 		;
+	// menu btn icon toggle
 	var menuBtnToggle = function(){
 		if ($mainMenu.hasClass("opened")){
 			$closeIcon.fadeOut(200);
@@ -17,29 +19,54 @@ $(document).ready(function(){
 			$closeIcon.fadeIn(200);
 		}
 	};
+	// menu open
+	var openMenu = function(){
+		$mainMenu.css("top", "100%");
+		$mainMenu.animate({
+			height: menuHeight
+		}, 200);
+		$mainMenu.addClass("opened");
+		menuBtnToggle();
+	};
+	// menu close
 	var closeMenu = function(){
-		$mainMenu.slideUp(200);
+		$mainMenu.animate({
+			height: "0"
+		}, 200, function(){
+			$mainMenu.css("top", "-9999px");
+		});
 		$mainMenu.removeClass("opened");
 		menuBtnToggle();
 	};
-	$menuBtn.on("click", function(){
-		$mainMenu.slideToggle(200);
-		$mainMenu.toggleClass("opened");
-		menuBtnToggle();
-		if ($mainMenu.hasClass("opened")){
-			var startCoor = $mainMenu.offset().top;
-			var menuScrollHandler = function (){
-				var currentCoor = $mainMenu.offset().top;
+	// menu close after some scroll
+	var closeAfterScroll = function(){
+		var  SCROLL_VAL_PX = 100	// close after 100px scroll
+			,startCoor = $mainMenu.offset().top
+			;
 
-				if (Math.abs(currentCoor - startCoor) >= 100){
-					closeMenu();
-					$(document).off("scroll", menuScrollHandler);
-				}
+		var menuScrollHandler = function (){
+			var currentCoor = $mainMenu.offset().top;
+
+			if (Math.abs(currentCoor - startCoor) >= SCROLL_VAL_PX){
+				closeMenu();
+				$(document).off("scroll", menuScrollHandler);
 			}
-			$(document).on("scroll", menuScrollHandler);
+		}
+		$(document).on("scroll", menuScrollHandler);
+	};
+
+	// open/close menue on mouseenter event
+	$menuBtn.on("mouseenter", function(){
+		if ($mainMenu.hasClass("opened")){
+			closeMenu();
+		} else {
+			openMenu();
+		};
+		if ($mainMenu.hasClass("opened")){
+			closeAfterScroll();
 		}
 	});
-
+	// close menu after click on close btn
 	$mainMenu.find(".js-menu__close").on("click", closeMenu);
 	
 	// mobile Footer accordion
