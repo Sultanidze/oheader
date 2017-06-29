b08861e2bebd932d6e9d93c57fdbe0b1e02a8345
Sultanidze <Sultanidze@gmail.com>
2017-06-29 19:21:51 +0300

    rating-prices done without adaptation

--- js/custom.js
+++ js/custom.js
@@ -2,89 +2,92 @@ $(document).ready(function(){
 	// stick header in top of window
 	$("#headerWrapper").stick_in_parent();
 
-	// menu hiding/appearance
-	var  $mainMenu = $("#mainMenu")
-		,$menuBtn = $("#menuBtn")
-		,$closeIcon = $menuBtn.find(".b-icon_bars")
-		,$openIcon = $menuBtn.find(".fa-times")
-		,menuHeight = $mainMenu.outerHeight();
-		;
-	// menu btn icon toggle
-	var menuBtnToggle = function(){
-		if ($mainMenu.hasClass("opened")){
-			$closeIcon.fadeOut(200);
-			$openIcon.fadeIn(200);
-		} else {
-			$openIcon.fadeOut(200);
-			$closeIcon.fadeIn(200);
-		}
-	};
-	// menu open
-	var openMenu = function(){
-		$mainMenu.css("top", "100%");
-		$mainMenu.animate({
-			height: menuHeight
-		}, 200);
-		$mainMenu.addClass("opened");
-		menuBtnToggle();
-	};
-	// menu close
-	var closeMenu = function(){
-		$mainMenu.animate({
-			height: "0"
-		}, 200, function(){
-			$mainMenu.css("top", "-9999px");
-		});
-		$mainMenu.removeClass("opened");
-		menuBtnToggle();
-	};
-	// menu close after some scroll
-	var closeAfterScroll = function(){
-		var  SCROLL_VAL_PX = 100	// close after 100px scroll
-			,startCoor = $mainMenu.offset().top
+	(function(){
+		"use strict"
+		// menu hiding/appearance
+		var  $mainMenu = $("#mainMenu")
+			,$menuBtn = $("#menuBtn")
+			,$closeIcon = $menuBtn.find(".b-icon_bars")
+			,$openIcon = $menuBtn.find(".fa-times")
+			,menuHeight = $mainMenu.outerHeight();
 			;
-
-		var menuScrollHandler = function (){
-			var currentCoor = $mainMenu.offset().top;
-
-			if (Math.abs(currentCoor - startCoor) >= SCROLL_VAL_PX){
-				closeMenu();
-				$(document).off("scroll", menuScrollHandler);
+		// menu btn icon toggle
+		var menuBtnToggle = function(){
+			if ($mainMenu.hasClass("opened")){
+				$closeIcon.fadeOut(200);
+				$openIcon.fadeIn(200);
+			} else {
+				$openIcon.fadeOut(200);
+				$closeIcon.fadeIn(200);
 			}
-		}
-		$(document).on("scroll", menuScrollHandler);
-	};
-
-	// open/close menue on mouseenter event
-	$menuBtn.on("mouseenter click", function(){
-		if ($mainMenu.hasClass("opened")){
-			closeMenu();
-		} else {
-			openMenu();
 		};
-		if ($mainMenu.hasClass("opened")){
-			closeAfterScroll();
-		}
-	});
-	// close menu after click on close btn
-	$mainMenu.find(".js-menu__close").on("click", closeMenu);
-	
-	// mobile Footer accordion
-	var $accordionTriggers = $("footer").find(".js-accordion__trigger");
-	var $accordionContents = $("footer").find(".js-accordion__content");
-	$accordionTriggers.on("click", function(){
-		var  $trigger = $(this)
-			,$content = $trigger.siblings(".js-accordion__content")
-			;
-		// close all opened accordion items except of clicked
-		$accordionContents.each(function(){
-			if (($(this)[0] !== $content[0])&&($(this).parent().hasClass("opened"))){
-				console.log("sdfsdff")
-				$(this).slideUp(200);
-				$(this).parent().removeClass("opened");
+		// menu open
+		var openMenu = function(){
+			$mainMenu.css("top", "100%");
+			$mainMenu.animate({
+				height: menuHeight
+			}, 200);
+			$mainMenu.addClass("opened");
+			menuBtnToggle();
+		};
+		// menu close
+		var closeMenu = function(){
+			$mainMenu.animate({
+				height: "0"
+			}, 200, function(){
+				$mainMenu.css("top", "-9999px");
+			});
+			$mainMenu.removeClass("opened");
+			menuBtnToggle();
+		};
+		// menu close after some scroll
+		var closeAfterScroll = function(){
+			var  SCROLL_VAL_PX = 100	// close after 100px scroll
+				,startCoor = $mainMenu.offset().top
+				;
+
+			var menuScrollHandler = function (){
+				var currentCoor = $mainMenu.offset().top;
+
+				if (Math.abs(currentCoor - startCoor) >= SCROLL_VAL_PX){
+					closeMenu();
+					$(document).off("scroll", menuScrollHandler);
+				}
+			}
+			$(document).on("scroll", menuScrollHandler);
+		};
+
+		// open/close menue on mouseenter event
+		$menuBtn.on("mouseenter click", function(){
+			if ($mainMenu.hasClass("opened")){
+				closeMenu();
+			} else {
+				openMenu();
+			};
+			if ($mainMenu.hasClass("opened")){
+				closeAfterScroll();
 			}
 		});
-		$content.slideToggle(200);
-		$trigger.parent().toggleClass("opened");
-	});
+		// close menu after click on close btn
+		$mainMenu.find(".js-menu__close").on("click", closeMenu);
+		
+		// mobile Footer accordion
+		var $accordionTriggers = $("footer").find(".js-accordion__trigger");
+		var $accordionContents = $("footer").find(".js-accordion__content");
+		$accordionTriggers.on("click", function(){
+			var  $trigger = $(this)
+				,$content = $trigger.siblings(".js-accordion__content")
+				;
+			// close all opened accordion items except of clicked
+			$accordionContents.each(function(){
+				if (($(this)[0] !== $content[0])&&($(this).parent().hasClass("opened"))){
+					console.log("sdfsdff")
+					$(this).slideUp(200);
+					$(this).parent().removeClass("opened");
+				}
+			});
+			$content.slideToggle(200);
+			$trigger.parent().toggleClass("opened");
+		});
+	})();
 });
