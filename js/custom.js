6acf7bdf2bb214077891c5a22a2541660151a04f
Sultanidze <Sultanidze@ukr.net>
2017-07-19 19:55:13 +0300

    slick slider bug after display:'none' in development

--- js/custom.js
+++ js/custom.js
@@ -146,7 +146,6 @@ $(document).ready(function(){
 				// close all opened accordion items except of clicked
 				obj.$accordionContents.each(function(){
 					if (($(this)[0] !== $content[0])&&($(this).parent().hasClass("opened"))){
-						console.log("sdfsdff")
 						$(this).slideUp(200);
 						$(this).parent().removeClass("opened");
 					}
@@ -163,12 +162,77 @@ $(document).ready(function(){
 		return obj;	// return object with menu methods and buttons
 	})();
 
-	// rating and specials sliders
+	// tabs module (in Rating, Specials)
+	var tabs = (function(){
+		// tabs init function
+		function init(oTabbed){
+			var sActiveTabClass = "b-btn_active js-tab_active";
+			oTabbed.$contents.each(function(){
+				if (!$(this).hasClass("js-content_active")){
+					
+					$(this).css("display", "none").removeClass("hidden");
+
+					// $(this).hide(0, function(){
+						// $(this).css("display", "none");
+					// });
+				}
+			});
+			oTabbed.$tabs.on("click", function(){
+				if ($(this).hasClass("js-tab_active")){
+					return false	// if active tab clicked do nothing
+				};
+				
+				if (!oTabbed.$contents.is(":animated")){
+					var index = oTabbed.$tabs.index(this);	// index of clicked tab
+
+					oTabbed.$tabs.removeClass(sActiveTabClass);
+					$(this).addClass(sActiveTabClass);
+					oTabbed.$contents.filter(".js-content_active").fadeOut(400, function(){
+						oTabbed.$contents.eq(index).addClass("js-content_active").fadeIn(400);
+					});
+				}
+			});
+		}
+		
+		var obj = {};
+
+		obj.rating = {};
+		obj.rating.$section = $("#rating");
+		obj.rating.$tabsBlock = obj.rating.$section.find(".js-tabsBlock");
+		obj.rating.$tabs = obj.rating.$tabsBlock.find(".js-tab");
+		obj.rating.$contentsBlock = obj.rating.$section.find(".js-tabContentsBlock");
+		obj.rating.$contents = obj.rating.$section.find(".js-content");
+		obj.rating.init = function(){
+			init(obj.rating);
+		};
+
+		obj.specials = {};
+		obj.specials.$section = $("#specials");
+		obj.specials.$tabsBlock = obj.specials.$section.find(".js-tabsBlock");
+		obj.specials.$tabs = obj.specials.$tabsBlock.find(".js-tab");
+		obj.specials.$contentsBlock = obj.specials.$section.find(".js-tabContentsBlock");
+		obj.specials.$contents = obj.specials.$section.find(".js-content");
+		obj.specials.init = function(){
+			init(obj.specials);
+		};
+
+		obj.init = function(){
+			obj.rating.init();	// rating tabs init
+			obj.specials.init();// specials tabs init
+		}
+
+		return obj;
+	})();
+
+	// sliders module
 	var sliders = (function(){
 		var obj = {};
 
-		obj.settingsRating = {
-			arrows: false,
+		obj.rating = {};	// rating slider object - contains '$slider' and 'settings' fields
+		obj.rating.settings = {
+			arrows: true,
+			// prevArrow: '$(this).parent().siblings(".js-slider_left")',
+			// nextArrow: '$(this).parent().siblings(".js-slider_right")',
 			infinite: true,
 			speed: 400,
 			slidesToShow: 3,
@@ -185,18 +249,55 @@ $(document).ready(function(){
 				{
 					breakpoint: 767,
 					settings: {
-					  slidesToShow: 1
+						slidesToShow: 1
 				  }
 				}
 			]
 		};
-		obj.$slidersRating = $(".js-slider__string_rating");
-		obj.slidersRatingInit = function(){
-			obj.$slidersRating.slick(obj.settingsRating);
+		obj.rating.$slider = $(".js-slider__string_rating");
+		// obj.rating.$prevBtn = obj.rating.$slider.parent().siblings(".js-slider_left");
+		// obj.rating.$nextBtn = obj.rating.$slider.parent().siblings(".js-slider_right");
+
+		// slider settings object extend function
+		function addBtnsSettings(oSlider){
+			var 
+				 t = this
+				,settings = {}
+				;
+
+			settings.prevArrow = $(t).parent().siblings(".js-slider_left");
+			settings.nextArrow = $(t).parent().siblings(".js-slider_right");
+
+			return	jQuery.extend(settings, oSlider.settings);
+		};
+		// init slider function
+		function init(oSlider){
+			oSlider.$slider.each(function(){
+				$(this).slick(addBtnsSettings.call(this, oSlider))
+			});
+		};
+		// init rating slider function
+		obj.rating.init = function(){
+		// 	// obj.rating.$slider.slick(function(){
+		// 	// 	var 
+		// 	// 		 t = this
+		// 	// 		,settings = {}
+		// 	// 		;
+
+		// 	// 	settings.prevArrow = $(t).parent().siblings(".js-slider_left");
+		// 	// 	settings.nextArrow = $(t).parent().siblings(".js-slider_right");
+
+		// 	// 	return	jQuery.extend(settings, obj.rating.settings);	
+		// 	// }.call(obj.rating.$slider));
+		// 	obj.rating.$slider.each(function(){
+		// 		$(this).slick(addBtnsSettings.call(this, obj.rating))
+		// 	});
+			init(obj.rating);
 		};
 
-		obj.settingsSpecials = {
-			arrows: false,
+		obj.specials = {};
+		obj.specials.settings = {
+			arrows: true,
 			infinite: true,
 			speed: 400,
 			slidesToShow: 2,
@@ -207,19 +308,19 @@ $(document).ready(function(){
 				{
 					breakpoint: 767,
 					settings: {
-					  slidesToShow: 1
+						slidesToShow: 1
 				  }
 				}
 			]
 		};
-		obj.$slidersSpecials = $(".js-slider__string_specials");
-		obj.slidersSpecialsInit = function(){
-			obj.$slidersSpecials.slick(obj.settingsSpecials);
+		obj.specials.$slider = $(".js-slider__string_specials");
+		obj.specials.init = function(){
+			init(obj.specials);
 		};
 
 		obj.init = function(){
-			obj.slidersRatingInit();
-			obj.slidersSpecialsInit();
+			obj.rating.init();
+			obj.specials.init();
 		}
 
 		return obj;	// return object with menu methods and buttons
@@ -259,8 +360,9 @@ $(document).ready(function(){
 	// executable part
 	header.init();	// header module init
 	indexContents.linkInit();	// content links functionality
-	sliders.init();	// footer module init
+	sliders.init();	// sliders module init
+	tabs.init();	// tabs init
 	footer.init();	// footer module init
 
-	seoMap.init();	// footer module init
+	seoMap.init();	// seoMap module init
 });
